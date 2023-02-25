df4c145917087afae264778137bb64ff2c8a6b37
Novakid <habashira90@gmail.com>
2023-02-25 11:33:21 -0600

    cambios en general y js

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
-//Aquí va el codigo javascript
+//Aquí va el codigo javascripv
+let Nombrefun = document.getElementById('Nombrefun'), 
+Tipofun = document.getElementById('Tipofun'), 
+descripfun = document.getElementById('descripfun');
+
+let boton = document.getElementById('capturar');
+
+boton.addEventListener("submit", myfunction)
+
+function myfunction(e){
+    e.preventDefault()
+    let Nombre = document.getElementById('Nombre').value;
+    let Tipo = document.getElementById('Tipo').value;
+    let descrip = document.getElementById('descrip').value;
+
+    Nombrefun.innerHTML = Nombre;
+    Tipofun.innerHTML = Tipo;
+    descripfun.innerHTML = descrip;
+}
